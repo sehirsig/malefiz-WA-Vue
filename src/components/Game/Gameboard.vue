bc9de81216c7343c768f8eb419d2b819283dedf8
Sebastian Hirsig <sebastian.hirsig@htwg-konstanz.de>
2022-12-11 23:25:10 +0100

    Init

---
+++ src/components/Game/Gameboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container p-0 m-1" id="gamepanel">
+    <div v-for="(n, i) in 18" :key="i" class="row justify-content-center">
+      <div v-for="(k, j) in 19" :key="j" class="col p-0 m-0">
+        <img class="game-field" :id="'field{'+ i +'}_{'+ j +'}'"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Gameboard"
+}
+</script>
+
+<style scoped>
+
+</style>
