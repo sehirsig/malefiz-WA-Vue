7a0b8dc1bc15a53a6002d153f9078e5cc7734267
Sebastian Hirsig <sebastian.hirsig@htwg-konstanz.de>
2022-12-14 18:35:09 +0100

    added: Game Modal to Vue File

---
+++ src/components/Navigation/GameInstruction.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="modal fade fixed-top" id="infoModal" tabindex="-1" role="dialog" aria-labelledby="infoModalLabel" aria-hidden="true" data-backdrop="false">
+    <div class="modal-dialog modal-dialog-centered" role="document" >
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="infoModalLabel">Game Instructions</h5>
+        </div>
+        <div class="modal-body">
+          <h3>Game Rules</h3>
+          <em>Malefiz is a board game for 2 to 4 players. Each player has 5 figures to play with. Every figure starts in their base, at the bottom of the board. The beginning player rolls the dice. The thrown number has to be pulled completly. While walking with the figure, change of direction is not allowed. If a player lands on an enemys figure, the enemy has to put his figure back to his base. Figures are allowed to jump over other figures, but not over barricades. If a player lands on the barricade (with the exact number), the barricade gets set anywhere on the game board, all black fields are allowed, except the lowest row.
+          </em>
+          <h4>Goal Of The Game</h4>
+          <em>The player reaching the top of the gameboard first (with the exact number rolled with the dice) with one figure wins the game.
+          </em>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GameInstruction"
+}
+</script>
+
+<style scoped>
+
+</style>
